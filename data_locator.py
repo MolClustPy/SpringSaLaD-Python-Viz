02c81fbd641e6f3cf1cf97f2f0dbb6e2034c8783
Hlpred <71465959+Hlpred@users.noreply.github.com>
2024-05-28 11:04:42 -0400

    Improved File Searching

--- data_locator.py
+++ data_locator.py
@@ -1,24 +1,26 @@
 import os
 import fnmatch
 
+#Finds the first txt file in the search_directory
 def find_txt_file(search_directory):
     for file in os.listdir(search_directory):
         if file.endswith(".txt"):
             return os.path.join(search_directory, file)
 
-def find_files(directory, search_string):
-    for root, dirs, files in os.walk(directory):
+#Finds the last file in the search_directory and its subdirecotires that matches the file_name
+def find_files(search_directory, file_name):
+    for root, dirs, files in os.walk(search_directory):
         for filename in files:
-            if fnmatch.fnmatch(filename, f"*{search_string}*"):
+            if fnmatch.fnmatch(filename, f"*{file_name}*"):
                 match = os.path.join(root, filename)
     return match
 
-def find_molecule_counts(search_directory, input_file = None):
-    if input_file == None:
-        input_file = find_txt_file(search_directory)
+#Finds the average molecule counts CSV for a given search_directory
+def find_molecule_counts(search_directory, file_name = None):
+    if file_name == None:
+        file_name = find_txt_file(search_directory)
     else:
-        input_file = search_directory + '\\' + input_file 
-    simulation_directory = input_file[:-4] + '_FOLDER'
-    search_directory = f'{simulation_directory}\data'
+        file_name = os.path.join(search_directory, file_name)
+    search_directory = os.path.join(file_name[:-4] + '_FOLDER', 'data')
     result = find_files(search_directory, 'AverageMoleculeCounts')
     return result
